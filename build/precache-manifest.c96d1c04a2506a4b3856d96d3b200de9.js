ef09c1a3860bc1006dd19a229ff2ab627382ab24
Tigran Yeranyan <tyeranyan@gmail.com>
2021-03-07 17:18:01 +0400

    build commit

---
+++ build/precache-manifest.c96d1c04a2506a4b3856d96d3b200de9.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc71859e46e4e166a58d9ee56911c824",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c9dd0c14e0de8f48d05",
+    "url": "/static/css/3.2e24d83a.chunk.css"
+  },
+  {
+    "revision": "a6e3c93844efd9f7fc62",
+    "url": "/static/css/main.7a4b675c.chunk.css"
+  },
+  {
+    "revision": "159dd72c7981df71c95d",
+    "url": "/static/js/0.130f355c.chunk.js"
+  },
+  {
+    "revision": "8c9dd0c14e0de8f48d05",
+    "url": "/static/js/3.2a1837e0.chunk.js"
+  },
+  {
+    "revision": "dd09aaf42f78ece6b931",
+    "url": "/static/js/4.a12c61e1.chunk.js"
+  },
+  {
+    "revision": "0da73a688a47199f534d",
+    "url": "/static/js/5.f5ecd6fd.chunk.js"
+  },
+  {
+    "revision": "8add712d7074aee045b0",
+    "url": "/static/js/6.980c31c3.chunk.js"
+  },
+  {
+    "revision": "18c207741a8a729d206a",
+    "url": "/static/js/7.8b8bd9c3.chunk.js"
+  },
+  {
+    "revision": "a6e3c93844efd9f7fc62",
+    "url": "/static/js/main.2ef4c1df.chunk.js"
+  },
+  {
+    "revision": "f789dca4b7e1641f26f3",
+    "url": "/static/js/runtime~main.eba6f5c1.js"
+  },
+  {
+    "revision": "b3d45c2d0f34f587655a2f3f2b181e01",
+    "url": "/static/media/Tour-Academy-Bus.b3d45c2d.gif"
+  },
+  {
+    "revision": "18084eaee3cc3bbfb2b222deee6eb6bd",
+    "url": "/static/media/blog.18084eae.svg"
+  },
+  {
+    "revision": "8e81dbabb25e5a2f29c0c9b5f79a74cf",
+    "url": "/static/media/call.8e81dbab.svg"
+  },
+  {
+    "revision": "ecaa5d1611ea8deb287993728dbaeeb1",
+    "url": "/static/media/catalog.ecaa5d16.svg"
+  },
+  {
+    "revision": "7d29fe45e4bddfb97928d70c1a84c9a8",
+    "url": "/static/media/item-img.7d29fe45.png"
+  },
+  {
+    "revision": "7e4cc3942f9aff2a271d2dd6413c4311",
+    "url": "/static/media/item.7e4cc394.png"
+  },
+  {
+    "revision": "b410db7c457e5009605d5e2826ccd46d",
+    "url": "/static/media/logo.b410db7c.svg"
+  },
+  {
+    "revision": "8b1ead27407608c9f6b2cb3cf1c7bc68",
+    "url": "/static/media/message.8b1ead27.svg"
+  },
+  {
+    "revision": "9c2a58251c97f017ec5aaaf0a858ade8",
+    "url": "/static/media/people.9c2a5825.svg"
+  },
+  {
+    "revision": "6fc62e96ada73abcc405355c88840add",
+    "url": "/static/media/social1.6fc62e96.svg"
+  },
+  {
+    "revision": "d0709de746ef4deac70605e0138bee35",
+    "url": "/static/media/social2.d0709de7.svg"
+  },
+  {
+    "revision": "d0709de746ef4deac70605e0138bee35",
+    "url": "/static/media/social3.d0709de7.svg"
+  },
+  {
+    "revision": "daf5070934607c9582b1fa91a9b997d9",
+    "url": "/static/media/social4.daf50709.svg"
+  },
+  {
+    "revision": "e3aba58f12ddfa3244056d37494189eb",
+    "url": "/static/media/social5.e3aba58f.svg"
+  },
+  {
+    "revision": "73b8f7db035aad5d3acad6cbdad31f83",
+    "url": "/static/media/social6.73b8f7db.svg"
+  },
+  {
+    "revision": "330c2eedbba4930000097993ea6f2821",
+    "url": "/static/media/top-grapes.330c2eed.svg"
+  }
+]);
